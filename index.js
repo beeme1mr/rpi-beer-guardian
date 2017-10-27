3323c48cd9a2251faa4881a66ff3ef24e7de1075
Michael Beemer <michael.beemer@dynatrace.com>
2017-10-27 14:11:57 -0400

    Wrapped the start method in an IIFE

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Motion = require("./motion");
 
-try {
-  const motion = new Motion(11);
-  motion.start();
-} catch (err) {
-  console.log(err);
-  process.exit(1);
-}
+const motion = new Motion(11);
+(async () => {
+  try {
+    console.log("Starting Raspberry Pi Beer Guardian controller")
+    await motion.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+})();
 
 process.on("SIGINT", async function() {
-  console.log("Exiting the script");
+  console.log("Shutting down the Beer Gaurdian controller");
   await motion.exit();
   process.exit();
 });
